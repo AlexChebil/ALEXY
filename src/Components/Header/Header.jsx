f0b4832aeb50335db7e8a3f2227be76957138cd7
Skander <skanderchebil1@gmail.com>
2022-06-01 16:26:30 +0100

    changed the search location from the header to the library component added some css and made the grid responsive

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,30 +1,11 @@
-import React, { useRef } from "react";
+import React from "react";
 import "./Header.scss";
-import TextField from "@mui/material/TextField";
 
 function Header() {
-  const searchInputRef = useRef();
-
-  /* Listening for the enter key to be pressed. */
-  window.addEventListener("keydown", (e) => {
-    if (e.key === "Enter") {
-      searchInputRef.current && console.log(searchInputRef.current.value);
-    }
-  });
-
   return (
     <>
       <div className='header'>
         <h1>ALEXY.</h1>
-
-        <div className='search'>
-          <TextField
-            inputRef={searchInputRef}
-            id='outlined-basic'
-            label='Search Movies'
-            variant='outlined'
-          />
-        </div>
       </div>
     </>
   );
--- src/Components/Librairy/Library.jsx
+++ src/Components/Librairy/Library.jsx
@@ -1,14 +1,18 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Movie from "./Movie/Movie";
 import "./Library.scss";
+import TextField from "@mui/material/TextField";
 
-function Library() {
+function Library({ searchValue }) {
+  const inputRef = useRef();
   const [movies, setMovies] = useState();
   const [sortBy, setSortBy] = useState("");
+  const [searchMovie, setSearchMovie] = useState("");
 
   const API_KEY = "7d92cb02e1baca465546cf38e2de00f0";
   const BASE_URL = "https://api.themoviedb.org/3";
   const REQUEST = `${BASE_URL}/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}`;
+  const SEARCH_REQUEST = `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${searchMovie}`;
 
   async function getMovies() {
     const rawData = await fetch(REQUEST);
@@ -19,10 +23,6 @@ function Library() {
     getMovies();
   }, []);
 
-  useEffect(() => {
-    sortFunction();
-  }, [sortBy]);
-
   function sortFunction() {
     if (sortBy === "rating") {
       setMovies(movies.sort((a, b) => a.vote_average < b.vote_average));
@@ -31,19 +31,46 @@ function Library() {
     }
   }
 
+  useEffect(() => {
+    sortFunction();
+  }, [sortBy]);
+
+  window.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") {
+      inputRef.current && setSearchMovie(inputRef.current.value);
+    }
+  });
+
   return (
     <>
-      <select
-        name='sortMovies'
-        id='sortMovies'
-        value={sortBy}
-        onChange={(e) => setSortBy(e.target.value)}
-      >
-        {/* inversed name and rating values because the render is delayed by a step
+      <div className='flexContainer'>
+        <div className='search'>
+          <TextField
+            inputRef={inputRef}
+            id='outlined-basic'
+            label='Search Movies'
+            variant='outlined'
+            color='warning'
+          />
+        </div>
+
+        <div className='selectDropdown'>
+          <select
+            name='sortMovies'
+            id='sortMovies'
+            value={sortBy}
+            onChange={(e) => setSortBy(e.target.value)}
+          >
+            {/* inversed name and rating values because the render is delayed by a step
         from the state */}
-        <option value='rating'>Name</option>
-        <option value='name'>Rating</option>
-      </select>
+            <option value='' disabled>
+              Sort BY
+            </option>
+            <option value='rating'>Name</option>
+            <option value='name'>Rating</option>
+          </select>
+        </div>
+      </div>
 
       <div className='movies'>
         {movies &&
