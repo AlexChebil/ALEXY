4825fbc29a2a12cba1e2c430c95efee03e7415c4
Skander <skanderchebil1@gmail.com>
2022-06-10 00:58:50 +0100

    added the website slogan to the header compo

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -6,6 +6,7 @@ function Header() {
     <>
       <div className='header'>
         <h1 onClick={() => window.location.reload()}>ALEXY.</h1>
+        <h1>Your Favorite Movies Picker Website.</h1>
       </div>
     </>
   );
